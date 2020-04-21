dedd58eac69178721d261be9fa97e676bce11eee
abc <abc@example.com>
2020-04-20 23:46:38 -0400

    Initial Commit. Created SpringBoot backend application configured application properties

---
+++ OnlineBooKStore/OnlineBooKStore/src/main/java/com/onlineBookStore/OnlineBooKStoreApplication.java
@@ -0,0 +1,13 @@
+package com.onlineBookStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBooKStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBooKStoreApplication.class, args);
+	}
+
+}
