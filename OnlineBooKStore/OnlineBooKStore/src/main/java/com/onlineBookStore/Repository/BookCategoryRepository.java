6ae802b4d163de8fa21845ccdb7c88e5e901757c
abc <abc@example.com>
2020-04-22 20:51:48 -0400

    Feature 1 - Completed
    -> created Angular Project
    -> Book list is displayed in the browser

--- OnlineBooKStore/OnlineBooKStore/src/main/java/com/onlineBookStore/Repository/BookCategoryRepository.java
+++ OnlineBooKStore/OnlineBooKStore/src/main/java/com/onlineBookStore/Repository/BookCategoryRepository.java
@@ -2,9 +2,11 @@ package com.onlineBookStore.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.onlineBookStore.Entity.BookCategory;
 
+@CrossOrigin("http://localhost:4200,*")
 @RepositoryRestResource(collectionResourceRel="BookCategory", path="book-category")
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
 
